4b229a373958c3706d56ba7eb6935535509ec392
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-10-07 18:00:41 +0900

    Update on the MainMenu

--- QviKD/MainMenu.xaml.cs
+++ QviKD/MainMenu.xaml.cs
@@ -28,62 +28,66 @@ namespace QviKD
         }
 
         private void MainMenu_Loaded(object sender, RoutedEventArgs e)
-        {   
-#if DEBUG
-            for(int index = 0; index < 6; index++)
+        {
+#if !DEBUG
+            for(int index = 0; index < 30; index++)
             {
-                if (index % MainMenuContent.ColumnDefinitions.Count == 0)
-                {
-                    MainMenuContent.RowDefinitions.Add(new RowDefinition()
-                    { 
-                        Height = new GridLength(0, GridUnitType.Auto),
-                    });
-                }
-
-                Button button = new()
-                {
-                    Margin = new Thickness(8),
-                };
-                button.SetValue(Grid.ColumnProperty, index % MainMenuContent.ColumnDefinitions.Count);
-                button.SetValue(Grid.RowProperty, index / MainMenuContent.ColumnDefinitions.Count);
-                MainMenuContent.Children.Add(button);
-
-                Binding binding = new("ActualWidth")
-                {
-                    RelativeSource = RelativeSource.Self,
-                };
-                button.SetBinding(HeightProperty, binding);
+                MainMenu_AppendButton(index);
             }
-
 #else
-            if (Database.Displays.Count > 0)
+            // Append buttons that each represents a monitor detected by the system.
+            for (int index = 0; index < Database.Displays.Count; index++)
             {
-                for (int index = 0; index < Database.Displays.Count; index++)
-                {
-                    if (index % MainMenuContent.ColumnDefinitions.Count == 0)
-                    {
-                        MainMenuContent.RowDefinitions.Add(new RowDefinition()
-                        {
-                            Height = new GridLength(0, GridUnitType.Auto),
-                        });
-                    }
-
-                    Button button = new()
-                    {
-                        Margin = new Thickness(8),
-                    };
-                    button.SetValue(Grid.ColumnProperty, index % MainMenuContent.ColumnDefinitions.Count);
-                    button.SetValue(Grid.RowProperty, index / MainMenuContent.ColumnDefinitions.Count);
-                    MainMenuContent.Children.Add(button);
-
-                    Binding binding = new("ActualWidth")
-                    {
-                        RelativeSource = RelativeSource.Self,
-                    };
-                    button.SetBinding(HeightProperty, binding);
-                }
+                MainMenu_AppendButton(index);
             }
+
+            // Append button that is for accessing modules that is not dependent to any monitor.
+            MainMenu_AppendButton(Database.Displays.Count, "Modules");
 #endif
+        }
+
+        private void MainMenu_AppendButton(int index, string content = null)
+        {
+            // Expand row of the grid if the index is out of range.
+            while (index / MainMenuContent.ColumnDefinitions.Count >= MainMenuContent.RowDefinitions.Count)
+            {
+                MainMenuContent.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = new GridLength(0, GridUnitType.Auto),
+                });
+            }
+
+            // Create a button and add it to the grid.
+            Button button = new()
+            {
+                Name = $"MainMenuButton{index}",
+                Margin = new Thickness(8),
+                Content = index < Database.Displays.Count && content == null ? Database.Displays[index].DeviceName : content,
+                MaxWidth = MainMenuContent.MaxWidth / MainMenuContent.ColumnDefinitions.Count * 0.8,
+                MaxHeight = MaxWidth,
+            };
+            button.SetValue(Grid.ColumnProperty, index % MainMenuContent.ColumnDefinitions.Count);
+            button.SetValue(Grid.RowProperty, index / MainMenuContent.ColumnDefinitions.Count);
+            _ = MainMenuContent.Children.Add(button);
+
+            // Square the button.
+            Binding binding = new("ActualWidth")
+            {
+                RelativeSource = RelativeSource.Self,
+            };
+            _ = button.SetBinding(HeightProperty, binding);
+
+            // Add Click event to the button.
+            button.Click += new RoutedEventHandler(MainMenu_ClickButton);
+        }
+
+        private void MainMenu_ClickButton(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            
+
+            _ = MessageBox.Show($"{button.Content}");
         }
     }
 }
--- QviKD/MonitorMenu.xaml.cs
+++ QviKD/MonitorMenu.xaml.cs
@@ -13,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using QviKDLib;
-
 namespace QviKD
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MonitorMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MonitorMenu : UserControl
     {
-        public MainWindow()
+        public MonitorMenu()
         {
             InitializeComponent();
-            _ = new EnumDisplays();
         }
     }
 }
