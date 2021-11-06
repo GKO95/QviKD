37faa1c92d2913be0868e615ebea6d2848cee25d
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2021-11-06 10:35:02 +0900

    Working on Custom Controls

--- QviKD/MainWindow.xaml.cs
+++ QviKD/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace QviKD
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AppWindow
     {
         public MainWindow()
         {
--- QviKD/Window.cs
+++ QviKD/Window.cs
@@ -16,35 +16,14 @@ using QviKD.Types;
 
 namespace QviKD
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:QviKD"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:QviKD;assembly=QviKD"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:ModuleWindow/>
-    ///
-    /// </summary>
+    public abstract class AppWindow : Window
+    {
+        static AppWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AppWindow), new FrameworkPropertyMetadata(typeof(AppWindow)));
+        }
+    }
+
     public abstract class ModuleWindow : Window
     {
         static ModuleWindow()
@@ -60,24 +39,31 @@ namespace QviKD
         // : for luminance measurement, calculator, et cetera.
         protected ModuleWindow()
         {
+            ModuleWindow_Constructor();
+            Display = null;
+
             WindowStyle = WindowStyle.SingleBorderWindow;
             WindowState = WindowState.Normal;
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
-
-            Display = null;
-            Loaded += ModuleWindow_Loaded;
         }
 
         // Constructor w/ Display information
         // : for calibration, pattern generator, et cetra.
         protected ModuleWindow(Display display)
         {
+            ModuleWindow_Constructor();
+            Display = display;
+
             WindowStyle = WindowStyle.None;
             WindowState = WindowState.Minimized;
             WindowStartupLocation = WindowStartupLocation.Manual;
+        }
 
-            Display = display;
-            Loaded += ModuleWindow_Loaded;            
+        private void ModuleWindow_Constructor()
+        {
+            Foreground = new SolidColorBrush(Colors.White);
+            Background = new SolidColorBrush(Colors.Transparent);
+            Loaded += ModuleWindow_Loaded;
         }
 
         protected virtual void ModuleWindow_Loaded(object sender, RoutedEventArgs e)
@@ -96,6 +82,11 @@ namespace QviKD
             }
         }
 
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
         protected static bool IsAvailable(string monitor, Type type)
         {
             if (type == typeof(WidgetPage))
@@ -105,6 +96,11 @@ namespace QviKD
                     && (Monitors.Count is 0 || Monitors.Contains(monitor));
         }
 
+        private void ModuleWindowCaptionButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
         /// <summary>
         /// Print message for debugging; DEBUG-mode exclusive.
         /// </summary>
@@ -112,6 +108,14 @@ namespace QviKD
         private void DebugMessage(string msg)
         {
             System.Diagnostics.Debug.WriteLine($"'{GetType().Name}.cs' {msg}");
+        }
+    }
+
+    public abstract class WidgetWindow : System.Windows.Window
+    {
+        static WidgetWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WidgetWindow), new FrameworkPropertyMetadata(typeof(WidgetWindow)));
         }
     }
 }
