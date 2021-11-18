7b2aa8451795bef472be887c402ac7e10c5952d4
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-11-18 11:17:05 +0900

    Minor script change

--- QvPattern/Patterns/GradientWhite256.xaml.cs
+++ QvPattern/Patterns/GradientWhite256.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QviKD.Modules.QvPattern
+namespace QviKD.Modules.QvPattern.Patterns
 {
     /// <summary>
     /// Interaction logic for GradientWhite256.xaml
