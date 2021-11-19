3b6033b9288fcf0b9521e0de65e9b0c7521ecd03
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2021-11-20 00:47:51 +0900

    Add SolidFullWhite pattern

---
+++ QvPattern/Patterns/SolidFullWhite.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QviKD.Modules.QvPattern.Patterns
+{
+    /// <summary>
+    /// Interaction logic for SolidFullWhite.xaml
+    /// </summary>
+    public partial class SolidFullWhite : Page
+    {
+        internal byte ucIndex = 255;
+        public SolidFullWhite()
+        {
+            InitializeComponent();
+            SolidFullWhiteContent.Background = new SolidColorBrush(Color.FromRgb(ucIndex, ucIndex, ucIndex));
+        }
+    }
+}
