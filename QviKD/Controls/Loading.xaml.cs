cc3458a1100ee880e199964b97e93a3c15fd1ba6
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-11-29 18:01:03 +0900

    Add Loading animation control

---
+++ QviKD/Controls/Loading.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace QviKD.Controls
+{
+    /// <summary>
+    /// Interaction logic for Loading.xaml
+    /// </summary>
+    public partial class Loading : UserControl
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+    }
+}
